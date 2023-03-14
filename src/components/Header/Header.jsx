a90802cbf3f838c8cda96cd0b6e3318fa348027e
Stilllee <orodae@gmail.com>
2023-03-15 03:07:10 +0900

    💄 : "logo, menu 스타일 작업"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import header from '@/assets/css/Header.module.css';
 
-function Header(props) {
-  return <h1>헤더입니다.</h1>;
+function Header() {
+  return (
+    <div className={header.header}>
+      <NavLink className={header.logo} to="/main" />
+      <ul className={header.menu}>
+        <li className={header.live}>실시간</li>
+        <li>TV프로그램</li>
+        <li className={header.movie}>영화</li>
+        <li className={header.paramount} />
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
