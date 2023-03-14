31c17530346de09bd59fc1b092e8f136a79f8543
Stilllee <orodae@gmail.com>
2023-03-14 14:12:09 +0900

    header 폴더 및 파일 생성에 따른 App.jsx파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '@components/Header/Header';
-import Footer from '@components/Footer/Footer';
-import Test from '@components/Test/Test';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
-        <Routes>
-          <Route path="/hello" element={<Test />} />
-        </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );
