31cc4b8b2b55e06cb8f7ddb66c3bcaf78c9ff667
luckydoyun <doyun.jang.0128@gmail.com>
2023-03-14 15:24:07 +0900

    💄 : "React vite 초기 환경설정" #83
    통합 index.css 생성 및 main.jsx에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-// import Header from './components/Header/Header';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import Test from '@components/Test/Test';
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <BrowserRouter>
