58af3825d60e7d70423681b17b6a79756134219a
Stilllee <orodae@gmail.com>
2023-03-16 06:20:09 +0900

    ✨ : "검색 아이콘 토글 기능 구현" #48
    검색 아이콘 클릭시 모달창 활성 및 비활성화 되는 토글 기능 구현
    
    Resolves : #48

--- src/components/Header/Aside.jsx
+++ src/components/Header/Aside.jsx
@@ -1,9 +1,23 @@
 import style from '@components/Header/Aside.module.css';
+import { useState } from 'react';
+import { Modal } from '@components';
 
 export function Aside() {
+  const [toggle, setToggle] = useState(false);
+
+  const onClickSearchHandler = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <aside className={style.aside}>
-      <button type="button" aria-label="Search" className={style.btn} />
+      <button
+        onClick={onClickSearchHandler}
+        type="button"
+        aria-label="검색"
+        className={style.btn}
+      />
+      {toggle === true && <Modal />}
       <div className={style.profile} />
     </aside>
   );
