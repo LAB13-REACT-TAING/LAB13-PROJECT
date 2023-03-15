42d3677aaf4c8599f1d640d32f2a2a83a39e7072
Stilllee <orodae@gmail.com>
2023-03-16 05:15:59 +0900

    ✨ : "logo 클릭시 main페이지로 이동" #47
    logo에 페이지 링크 연결
    
    Resolves : #47

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Menu, Aside } from '@components';
 function Header() {
   return (
     <div className={style.header}>
-      <NavLink className={style.logo} />
+      <NavLink className={style.logo} to="/main" />
       <Menu />
       <Aside />
     </div>
