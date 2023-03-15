635ae64a579397471535d4159ac3707b02c65f10
Stilllee <orodae@gmail.com>
2023-03-16 05:06:42 +0900

    💄 : "헤더 마크업 및 스타일 작업"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '@components/Header/Header';
+import Header from '@pages/Header/Header';
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export * from './Header/Menu';
+export * from './Header/Aside';
