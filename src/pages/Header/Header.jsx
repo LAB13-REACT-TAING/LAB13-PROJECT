3cf77142f45d50308958b39b8f57a5c9eee92e19
Stilllee <orodae@gmail.com>
2023-03-16 05:12:44 +0900

    💄 : "헤더 마크업 및 스타일 작업"

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Menu, Aside } from '@components';
 function Header() {
   return (
     <div className={style.header}>
-      <NavLink className={style.logo} to="/main" />
+      <NavLink className={style.logo} />
       <Menu />
       <Aside />
     </div>
