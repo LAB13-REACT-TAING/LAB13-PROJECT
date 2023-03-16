552a249aea8766c573009ab4428610ca94881be5
Stilllee <orodae@gmail.com>
2023-03-16 21:38:35 +0900

    💄 : "프로필 마크업 및 스타일 적용"

---
+++ src/components/Header/Profile.jsx
@@ -0,0 +1,28 @@
+import style from '@components/Header/Profile.module.css';
+
+// icon ? `${style.modal}` : `${style.modal}` + `${style.hidden}`
+
+export function Profile() {
+  return (
+    <div>
+      <div className={style.member} />
+      <div className={style.my}>
+        <div className={style.profile}>
+          <div className={style.image}></div>
+          <div className={style.info}>
+            <p className={style.name}>닉네임</p>
+            <button className={style.site}>프로필 전환 ></button>
+          </div>
+        </div>
+        <hr />
+        <ul className={style.menu}>
+          <li>MY</li>
+          <li>이용권</li>
+          <li>쿠폰등록</li>
+          <li>고객센터</li>
+          <li>로그아웃</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
