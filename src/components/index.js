f0233bd9a27224cc6eca35433e41c1f88f60fdb4
Stilllee <orodae@gmail.com>
2023-03-16 21:38:35 +0900

    💄 : "프로필 마크업 및 스타일 적용"

--- src/components/index.js
+++ src/components/index.js
@@ -18,3 +18,5 @@ export * from './Footer/TopInfo';
 export * from './Header/Menu';
 export * from './Header/Aside';
 export * from './Header/Modal';
+// eslint-disable-next-line import/export
+export * from './Header/Profile';
