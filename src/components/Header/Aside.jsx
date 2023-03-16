2b23931ffa95955e5d59152331ccdcc546d2f14b
Stilllee <orodae@gmail.com>
2023-03-16 21:38:35 +0900

    💄 : "프로필 마크업 및 스타일 적용"

--- src/components/Header/Aside.jsx
+++ src/components/Header/Aside.jsx
@@ -1,14 +1,23 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/mouse-events-have-key-events */
 import style from '@components/Header/Aside.module.css';
 import { useState } from 'react';
-import { Modal } from '@components';
+// eslint-disable-next-line no-unused-vars
+import { Modal, Profile } from '@components';
 
 export function Aside() {
   const [toggle, setToggle] = useState(false);
-  const [icon, setIcon] = useState(true);
+  const [icon, setIcon] = useState(false);
+  const [show, setShow] = useState(false);
 
   const onClickSearchHandler = () => {
     setToggle(!toggle);
     setIcon(!icon);
+  };
+
+  const onClickProfileHandler = () => {
+    setShow(!show);
   };
 
   return (
@@ -17,11 +26,11 @@ export function Aside() {
         onClick={onClickSearchHandler}
         type="button"
         aria-label="검색"
-        className={icon ? `${style.searchBtn}` : `${style.closeBtn}`}
+        className={icon ? `${style.closeBtn}` : `${style.searchBtn}`}
       />
-
+      {show === true && <Profile />}
+      <div onClick={onClickProfileHandler} className={style.profile} />
       {toggle === true && <Modal />}
-      <div className={style.profile} />
     </aside>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export * from './Header/Menu';
 export * from './Header/Aside';
 export * from './Header/Modal';
+// eslint-disable-next-line import/export
+export * from './Header/Profile';
