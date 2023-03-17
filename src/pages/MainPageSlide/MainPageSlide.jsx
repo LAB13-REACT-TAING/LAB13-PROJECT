2e24fb09085c641efc2df22d467158d101bb5800
kyoungholee <dlrudgh1303@gmail.com>
2023-03-18 00:21:16 +0900

    📝 : "메인 페이지 슬라이드[티빙에서 꼭 봐야하는 콘텐츠] 기능완료 " #17

--- src/pages/MainPageSlide/MainPageSlide.jsx
+++ src/pages/MainPageSlide/MainPageSlide.jsx
@@ -2,27 +2,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { useEffect, useState } from 'react';
 import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
 import { Link, useParams, useNavigate } from 'react-router-dom';
-import style from './MainPageSlide.module.css';
 import './MainPageSlide.css';
 import 'swiper/swiper.css';
 import 'swiper/css/navigation';
-
 import { useReadData } from '../../utils/firebase/index';
-
-// eslint-disable-next-line import/no-duplicates
-import contentsMovie01 from '../../assets/images/amugeosdo_02.jpg';
-import contentsMovie02 from '../../assets/images/alsseurinjap_02.jpg';
-import contentsMovie03 from '../../assets/images/blackdog_02.jpg';
-import contentsMovie04 from '../../assets/images/bomulchajgi_02.jpg';
-import contentsMovie05 from '../../assets/images/canadacheckin_02.jpg';
-import contentsMovie06 from '../../assets/images/hwanhon_02.jpg';
-import contentsMovie07 from '../../assets/images/killimanjaro_02.jpg';
-import contentsMovie08 from '../../assets/images/maknaeadeul_02.jpg';
-import contentsMovie09 from '../../assets/images/nollauntoyoil_02.jpg';
-import contentsMovie10 from '../../assets/images/showmethemoney_02.jpg';
-import contentsMovie11 from '../../assets/images/sulkkundosi_02.jpg';
-import contentsMovie12 from '../../assets/images/teolsaking_02.jpg';
-import contentsMovie13 from '../../assets/images/urideurui_02.jpg';
 import QuickVodSlide from '../QuickVodSlide/QuickVodSlide';
 import PopularProgramSlide from '../PopularProgramSlide/PopularProgramSlide';
 import LiveChannel from '../LiveChannel/LiveChannel';
@@ -33,123 +16,51 @@ import EventSlide from '../EventSlide/EventSlide';
 SwiperCore.use([Navigation, Pagination, A11y]);
 
 export default function MainPageSlide() {
-  const navigationPrevRef = React.useRef(null);
-  const navigationNextRef = React.useRef(null);
-
-  // const navigate = useNavigate();
-  const { id = 4 } = useParams();
-
-  const [num, setNum] = useState(1);
-
+  const [filterData, setfilterData] = useState([]);
   const { readData, data, isLoading, error } = useReadData('image');
+  const baseUrl = '../src/assets/images/'; // 경로 설정
 
   async function handleReadData() {
     // 모든 데이터를 가져옵니다.
     await readData();
-
-    // 또는 특정 도큐멘트 데이터만 가져옵니다.
-    await readData();
   }
-
-  const TestHandler = () => {
-    navigationPrevRef.current = undefined;
-    navigationNextRef.current = undefined;
-    setNum(num + 2);
-
-    // console.log(navigationPrevRef);
-  };
-
-  const Test1Handler = () => {
-    navigationPrevRef.current = null;
-    navigationNextRef.current = null;
-    setNum(num + 2);
-  };
+  // 필요한 데이터만 뽑기
+  useEffect(() => {
+    readData();
+    const result = data?.filter(ele => ele.src.slide);
+    setfilterData(result);
+  }, []);
 
   return (
     <>
-      <div className={style.mainBanner}>메인 배너 들어갈 자리 </div>
-      <div
-        className="contanier-title"
-        onMouseEnter={TestHandler}
-        onMouseLeave={Test1Handler}
-      >
+      <div className="swiper-title">
         <h2
           style={{ 'margin-left': '60px', 'margin-bottom': '8px' }}
-          className={style.list_title}
+          className="list_title"
         >
           티빙에서 꼭 봐야하는 콘텐츠
         </h2>
 
         <Swiper
-          style={{ 'margin-bottom': '36px' }}
+          style={{ 'margin-bottom': '36px', 'margin-left': '44px' }}
           spaceBetween={10}
-          slidesPerView={7.2}
+          slidesPerView={7.4}
           slidesPerGroup={6}
-          navigation={{
-            prevEl: navigationPrevRef.current,
-            nextEl: navigationNextRef.current,
-          }}
+          navigation
         >
-          <SwiperSlide style={{ 'margin-left': '40px' }}>
-            <div className={style.contentsImg}>
-              <Link to={`/detailPage01/${id}`} className={style.content_name}>
-                <img src={contentsMovie02} alt="이미지 사진" />
-                아무것도 하기 싫다.
-              </Link>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <img src={contentsMovie02} alt="이미지 사진" />
-            <div>
-              <p className={style.content_name}>알쓸인잡</p>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie03} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie04} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie05} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie06} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie07} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie08} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie09} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie10} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie11} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie12} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie13} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie01} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie02} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie03} alt="이미지 사진" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={contentsMovie04} alt="이미지 사진" />
-          </SwiperSlide>
+          {filterData?.map(contents => (
+            <SwiperSlide>
+              <div>
+                <Link to={`${contents.src.slide}/${contents.id}`}>
+                  <img
+                    src={`${baseUrl}${contents.src.slide}.jpg`}
+                    alt={`${contents.name}`}
+                  />
+                  <p className="contentsName">{contents.name}</p>
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
       <QuickVodSlide />
--- src/pages/QuickVodSlide/QuickVodSlide.jsx
+++ src/pages/QuickVodSlide/QuickVodSlide.jsx
@@ -1,16 +1,8 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { useEffect, useState } from 'react';
 import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
-import styles from './QuickVodSlide.module.css';
 import 'swiper/swiper.css';
 import 'swiper/css/navigation';
-
-import quickVod01 from '../../assets/images/almaengi_02.jpg';
-import quickVod02 from '../../assets/images/chainaneun _02.jpg';
-import quickVod03 from '../../assets/images/eommauibomnal_02.jpg';
-import quickVod04 from '../../assets/images/geongangmyeonsae_02.jpg';
-import quickVod05 from '../../assets/images/jtbcnewsroom_02.jpg';
-import quickVod06 from '../../assets/images/narosalgiro_02.jpg';
 
 SwiperCore.use([Navigation, Pagination, A11y]);
 
@@ -18,10 +10,7 @@ export default function QuickVodSlide() {
   return (
     <>
       <div>
-        <h2
-          style={{ 'margin-left': '60px', 'margin-bottom': '12px' }}
-          className={styles.list_title}
-        >
+        <h2 style={{ 'margin-left': '60px', 'margin-bottom': '12px' }}>
           Quick VOD
         </h2>
       </div>
@@ -35,23 +24,7 @@ export default function QuickVodSlide() {
         pagination={{ clickable: true }}
       >
         <SwiperSlide style={{ 'margin-left': '40px' }}>
-          <img src={quickVod01} alt="이미지 사진" />
-        </SwiperSlide>
-
-        <SwiperSlide style={{ 'margin-right': '4px' }}>
-          <img src={quickVod02} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide style={{ 'margin-right': '0' }}>
-          <img src={quickVod03} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={quickVod04} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={quickVod05} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={quickVod06} alt="이미지 사진" />
+          {/* <img src={"../"} alt="이미지 사진" /> */}
         </SwiperSlide>
       </Swiper>
     </>
