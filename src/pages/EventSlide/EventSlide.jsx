88dc980668f5b9781d508849f8fd3c4def814a43
kyoungholee <dlrudgh1303@gmail.com>
2023-03-20 16:08:14 +0900

    ♻️ : "메인페이지 소제목별 슬라이드 기능 구현 완료" #20

--- src/pages/EventSlide/EventSlide.jsx
+++ src/pages/EventSlide/EventSlide.jsx
@@ -1,58 +1,69 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import React, { useEffect, useState } from 'react';
 import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
-import styles from './EventSlide.module.css';
+import { Link } from 'react-router-dom';
 import 'swiper/swiper.css';
 import 'swiper/css/navigation';
-
-import eventSlide01 from '../../assets/images/smallbanner01_03.jpg';
-import eventSlide02 from '../../assets/images/smallbanner02_03.jpg';
-import eventSlide03 from '../../assets/images/smallbanner03_03.jpg';
-import eventSlide04 from '../../assets/images/smallbanner04_03.jpg';
-import eventSlide05 from '../../assets/images/smallbanner05_03.jpg';
-import eventSlide06 from '../../assets/images/smallbanner06_03.jpg';
+import { useReadData } from '../../utils/firebase/index';
+import './EventSlide.css';
 
 SwiperCore.use([Navigation, Pagination, A11y]);
 
 export default function EventSlide() {
+  const baseUrl = '../src/assets/images/'; // 경로 설정
+  const [filterData, setfilterData] = useState([]);
+  const { readData, data, isLoading, error } = useReadData('image');
+  useEffect(() => {
+    (async () => {
+      if (!data) {
+        await readData();
+      }
+    })();
+
+    if (data) {
+      localStorage.setItem('eventState', JSON.stringify({ image: data }));
+      const records = JSON.parse(localStorage.getItem('eventState'));
+      const datas = records?.image?.filter(item => {
+        return item.src?.smallbanner !== undefined;
+      });
+
+      setfilterData(datas);
+    }
+  }, [data, readData]);
   return (
     <>
       <div>
         <h2
-          style={{ 'margin-left': '60px', 'margin-bottom': '12px' }}
-          className={styles.list_title}
+          style={{ 'margin-left': '60px', 'margin-bottom': '8px' }}
+          className="list_title"
         >
           이벤트
         </h2>
       </div>
 
       <Swiper
-        style={{ 'margin-bottom': '36px' }}
+        style={{
+          'margin-bottom': '36px',
+          'margin-left': '44px',
+          'padding-top': '10px',
+        }}
         spaceBetween={10}
         slidesPerView={5.2}
         slidesPerGroup={5}
         navigation
-        pagination={{ clickable: true }}
       >
-        <SwiperSlide style={{ 'margin-left': '40px' }}>
-          <img src={eventSlide01} alt="이미지 사진" />
-        </SwiperSlide>
-
-        <SwiperSlide style={{ 'margin-right': '4px' }}>
-          <img src={eventSlide02} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide style={{ 'margin-right': '0' }}>
-          <img src={eventSlide03} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={eventSlide04} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={eventSlide05} alt="이미지 사진" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src={eventSlide06} alt="이미지 사진" />
-        </SwiperSlide>
+        {filterData?.map(contents => (
+          <SwiperSlide key={contents.id}>
+            <div>
+              <Link to={`${contents.src.smallbanner}/${contents.id}`}>
+                <img
+                  src={`${baseUrl}${contents.src.smallbanner}.jpg`}
+                  alt={`${contents.name}`}
+                />
+              </Link>
+            </div>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </>
   );
--- src/pages/MainPageSlide/MainPageSlide.jsx
+++ src/pages/MainPageSlide/MainPageSlide.jsx
@@ -5,31 +5,20 @@ import { Link } from 'react-router-dom';
 import './MainPageSlide.css';
 import 'swiper/swiper.css';
 import 'swiper/css/navigation';
-import { useReadData } from '../../utils/firebase/index';
 import QuickVodSlide from '../QuickVodSlide/QuickVodSlide';
 import PopularProgramSlide from '../PopularProgramSlide/PopularProgramSlide';
 import LiveChannel from '../LiveChannel/LiveChannel';
 import OnlyTaing from '../OnlyTaing/OnlyTaing';
 import Advertisement from '../Advertisement/Advertisement';
 import EventSlide from '../EventSlide/EventSlide';
+import useDataFilter from '../../hooks/useDataFilter';
 
 SwiperCore.use([Navigation, Pagination, A11y]);
 
 export default function MainPageSlide() {
-  const [filterData, setfilterData] = useState([]);
-  const { readData, data, isLoading, error } = useReadData('image');
   const baseUrl = '../src/assets/images/'; // 경로 설정
 
-  async function handleReadData() {
-    // 모든 데이터를 가져옵니다.
-    await readData();
-  }
-  // 필요한 데이터만 뽑기
-  useEffect(() => {
-    readData();
-    const result = data?.filter(ele => ele.src.slide);
-    setfilterData(result);
-  }, []);
+  const FilterData = useDataFilter();
 
   return (
     <>
@@ -45,15 +34,15 @@ export default function MainPageSlide() {
           style={{
             'margin-bottom': '36px',
             'margin-left': '44px',
-            'margin-top': '10px',
+            'padding-top': '10px',
           }}
           spaceBetween={10}
           slidesPerView={7.4}
           slidesPerGroup={6}
           navigation
         >
-          {filterData?.map(contents => (
-            <SwiperSlide>
+          {FilterData?.map(contents => (
+            <SwiperSlide key={contents.id}>
               <div>
                 <Link to={`${contents.src.slide}/${contents.id}`}>
                   <img
