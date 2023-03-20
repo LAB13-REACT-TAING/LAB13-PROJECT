0d939c5339d057847d93da6e3b05b905e17544a0
chaewon <juh7814@gmail.com>
2023-03-20 17:22:04 +0900

    💡 : "useDataFilter 커스텀 훅 생성"
    공통 작업을 위해 필요한 훅 생성

--- src/hooks/useDataFilter.jsx
+++ src/hooks/useDataFilter.jsx
@@ -3,10 +3,10 @@ import { useReadData } from '../utils/firebase/firestore/index';
 
 /**
  *
- * @param {string} area 데이터 상세 링크 정보
+ * @param {string} imageDataValue 데이터 상세 링크 정보
  */
 
-export default function useDataFilter(area) {
+export default function useDataFilter(imageDataValue) {
   const [filterData, setfilterData] = useState([]);
   const { readData, data, isLoading, error } = useReadData('image');
   useEffect(() => {
@@ -19,19 +19,15 @@ export default function useDataFilter(area) {
     if (data) {
       localStorage.setItem('imageState', JSON.stringify({ image: data }));
       const records = JSON.parse(localStorage.getItem('imageState'));
-      const datas = records?.image
-        ?.filter(item => {
-          return item.src?.area !== undefined;
-        })
-        .map(item => {
-          return item.src?.area;
-        });
+      const datas = records?.image?.filter(item => {
+        return item.src?.[imageDataValue] !== undefined;
+      });
+
       setfilterData(datas);
     }
-  }, [data, readData]);
+  }, [data, imageDataValue, readData]);
 
   console.log(filterData);
-  //   console.log(filterData) 삭제해야됨;
 
-  return setfilterData;
-}
+  return filterData;
+}; 
