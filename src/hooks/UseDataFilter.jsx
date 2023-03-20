c3ddfcaac9a1abf401cbb68a0727320409acf485
ddengmin <ammin1127@naver.com>
2023-03-21 00:38:05 +0900

    ♻️ "코드 리팩토링" css 적용문제 해결중

--- src/hooks/UseDataFilter.jsx
+++ src/hooks/UseDataFilter.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useReadData } from '../utils/firebase/firestore';
 
-export default function useDataMain() {
+/**
+ *
+ * @param {string} imageDataValue 데이터 상세 링크 정보
+ */
+
+export default function useDataFilter(imageDataValue) {
   const [filterData, setfilterData] = useState([]);
   const { readData, data, isLoading, error } = useReadData('image');
   useEffect(() => {
@@ -14,19 +19,13 @@ export default function useDataMain() {
     if (data) {
       localStorage.setItem('imageState', JSON.stringify({ image: data }));
       const records = JSON.parse(localStorage.getItem('imageState'));
-      const datas = records?.image
-        ?.filter(item => {
-          return item.src?.main !== undefined;
-        })
-        .map(item => {
-          return item.src?.main;
-        });
+      const datas = records?.image?.filter(item => {
+        return item.src?.[imageDataValue] !== undefined;
+      });
 
       setfilterData(datas);
     }
-  }, [data, readData]);
-
-  console.log(filterData);
+  }, [data, imageDataValue, readData]);
 
   return filterData;
 }
--- src/pages/MainNavPage/MainNavPage.jsx
+++ src/pages/MainNavPage/MainNavPage.jsx
@@ -1,6 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
-import useDataMain from '../../hooks/useDataMain';
+import UseDataFilter from '../../hooks/UseDataFilter';
 
 import 'swiper/swiper.css';
 import 'swiper/css/navigation';
@@ -8,27 +8,29 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import style from './MainNavPage.module.css';
 
-export default function UseDataFilter() {
-  const imagedatas = useDataMain();
+export default function UseDataMain() {
+  const baseUrl = '../src/assets/images/';
+  const FilterData = UseDataFilter('main');
+
   const slides = [
     {
       id: 'slide-1',
-      description: '테스트용1',
+      description: '인생 2회차를 사는 판타지 드라마',
       link: '/sample/1',
     },
     {
       id: 'slide-2',
-      description: '테스트용2',
+      description: '주말 저녁 온 가족이 함께 즐길 수 있는 웃음 라인업',
       link: '/sample/2',
     },
     {
       id: 'slide-3',
-      description: '테스트용3',
+      description: '하루 끝의 술 한잔이 인생의 신념인 본격 기승전술 드라마',
       link: '/sample/3',
     },
     {
       id: 'slide-4',
-      description: '테스트용4',
+      description: '유재석, 조세호의 토크쇼&퀴즈쇼 프로그램',
       link: '/sample/4',
     },
   ];
@@ -40,32 +42,31 @@ export default function UseDataFilter() {
         slidesPerView={1}
         spaceBetween={0}
         speed={400}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        onMouseEnter={{
-          pauseOnMouseEnter: true,
-        }}
         loop
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         className={style.swiper}
       >
-        {imagedatas.map(url => {
-          const baseUrl = '../src/assets/images/';
-          const src = `${baseUrl}${url}.jpg`;
+        {FilterData.map((contents, index) => {
+          const slide = slides[index];
+
           return (
-            <SwiperSlide key={slides.id} className={style.swiperSlide}>
-              <img src={src} alt="이미지" />
-              <div className={style.contents}>
-                <p className={style.contents_text}>{slides.description}</p>
-                <a className={style.contents_button} href={slides.link}>
-                  자세히 보기
-                </a>
-              </div>
+            <SwiperSlide key={contents.id} className={style.swiperSlide}>
+              <section className={style.section}>
+                <img
+                  src={`${baseUrl}${contents.src.main}.jpg`}
+                  alt={`${contents.name}`}
+                />
+              </section>
+              {slide && (
+                <div className={style.contents}>
+                  <p className={style.contents_text}>{slide.description}</p>
+                  <a className={style.contents_button} href={slide.link}>
+                    자세히 보기
+                  </a>
+                </div>
+              )}
             </SwiperSlide>
           );
         })}
