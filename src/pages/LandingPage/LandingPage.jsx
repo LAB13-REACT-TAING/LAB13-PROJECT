aea8efb977ff4782b9cfcdea44e013cb18fccd56
chaewon <juh7814@gmail.com>
2023-03-20 17:19:56 +0900

    ✏️ : "공통 요소 객체로 묶음" #4, #5

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,27 +4,29 @@ import LandingSlideTitle from '@components/LandingSlide/LandingSlideTitle';
 import style from './LandingPage.module.css';
 
 export default function LandingPage() {
-  const mainTitle = '타잉에만 있는 재미';
-  const mainBoldText = '오리지널 콘텐츠를 만나보세요!';
-  const mainText = '차별화된 웰메이드 오리지널 콘텐츠';
-
-  const autoTitle = '내가 찾던 재미';
-  const autoBoldText = '보고 싶은 콘텐츠를 발견하세요!';
-  const autoText =
-    '최신, 인기 TV프로그램, 영화, 해외시리즈, 파라마운트+ 오리지널 및 독점';
+  const mainTitle = {
+    title: '타잉에만 있는 재미',
+    boldText: '오리지널 콘텐츠를 만나보세요!',
+    text: '차별화된 웰메이드 오리지널 콘텐츠',
+  };
+  const autoTitle = {
+    title: '내가 찾던 재미',
+    boldText: '보고 싶은 콘텐츠를 발견하세요!',
+    text: '최신, 인기 TV프로그램, 영화, 해외시리즈, 파라마운트+ 오리지널 및 독점',
+  };
 
   return (
     <div className={style.landing}>
       <LandingSlideTitle
-        title={mainTitle}
-        boldText={mainBoldText}
-        text={mainText}
+        title={mainTitle.title}
+        boldText={mainTitle.boldText}
+        text={mainTitle.text}
       />
       <LandingMainSlide />
       <LandingSlideTitle
-        title={autoTitle}
-        boldText={autoBoldText}
-        text={autoText}
+        title={autoTitle.title}
+        boldText={autoTitle.boldText}
+        text={autoTitle.text}
       />
       <LandingAutoSlide />
     </div>
