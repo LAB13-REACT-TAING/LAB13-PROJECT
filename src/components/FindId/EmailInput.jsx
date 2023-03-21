ffe8a2f8fdeb9e11770bdf1620e7cc4b7eba82a6
chaewon <juh7814@gmail.com>
2023-03-21 21:48:28 +0900

    ⚡️ : "input 함수 추가" #68

--- src/components/FindId/EmailInput.jsx
+++ src/components/FindId/EmailInput.jsx
@@ -1,12 +1,24 @@
+import { useState } from 'react';
 import style from './EmailInput.module.css';
 
 export default function EmailInput() {
+  const [email, setEmail] = useState('');
+
+  const onChangeHandler = e => {
+    setEmail(e.target.value);
+  };
+  const onClickHandler = () => {
+    alert('체크');
+  };
+
   return (
     <fieldset className={style.email}>
       <h3 className={style.title}>이메일로 찾기</h3>
       <p className={style.info}>가입 시 등록한 이메일을 입력해주세요.</p>
       <label htmlFor="findId" name="email">
         <input
+          value={email}
+          onChange={onChangeHandler}
           id="email"
           type="email"
           placeholder="이메일"
@@ -14,7 +26,7 @@ export default function EmailInput() {
           required
         />
       </label>
-      <button className={style.button} type="button">
+      <button onClick={onClickHandler} className={style.button} type="submit">
         확인
       </button>
     </fieldset>
