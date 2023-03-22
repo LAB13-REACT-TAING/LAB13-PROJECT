becd3dd880b88d7705e6db52b7de92ce35facba8
chaewon <juh7814@gmail.com>
2023-03-22 14:19:11 +0900

    🐛 : "useResetPassword 공통 코드 삭제" #72

--- src/utils/firebase/auth/useResetPassword.js
+++ src/utils/firebase/auth/useResetPassword.js
@@ -1,25 +1,23 @@
 import { sendPasswordResetEmail, getAuth } from 'firebase/auth';
 import { useCallback, useMemo, useState } from 'react';
+// eslint-disable-next-line import/no-cycle
+import { auth } from './index';
 
 export function useResetPassword() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const auth = getAuth();
 
-  const resetPassword = useCallback(
-    async email => {
-      setIsLoading(true);
-      try {
-        await sendPasswordResetEmail(auth, email);
-        // eslint-disable-next-line no-shadow
-      } catch (error) {
-        setError(error);
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [auth],
-  );
+  const resetPassword = useCallback(async email => {
+    setIsLoading(true);
+    try {
+      await sendPasswordResetEmail(auth, email);
+      // eslint-disable-next-line no-shadow
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
   return useMemo(
     () => ({
