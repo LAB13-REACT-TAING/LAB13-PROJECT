e44d1aa069c7b35dbd2bd6353bce011c9f7c78dc
chaewon <juh7814@gmail.com>
2023-03-23 20:55:18 +0900

    🚚 : "비밀번호 찾기 path 추가 " #71, #72, #73, #74

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '@components/Header/Header';
 import Footer from '@components/Footer/Footer';
 import Test from '@components/Test/Test';
-import FindPasswordPage from './pages/FindPassword/FindPasswordPage';
+import FindPasswordPage from '@pages/FindPassword/FindPasswordPage';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/hello" element={<FindPasswordPage />} />
+          <Route path="/findpassword" element={<FindPasswordPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/pages/FindPassword/FindPasswordPage.jsx
+++ src/pages/FindPassword/FindPasswordPage.jsx
@@ -2,9 +2,9 @@ import FindPassword from '@components/FindPassword/FindPassword';
 import style from './FindPasswordPage.module.css';
 
 export default function FindPasswordPage() {
-    return (
-      <main className={style.main}>
-        <FindPassword />
-      </main>
-    );
-  }
+  return (
+    <main className={style.main}>
+      <FindPassword />
+    </main>
+  );
+}
