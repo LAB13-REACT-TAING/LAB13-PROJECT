8db8f6b77cd417c07d1d36fa2c8aa25e176860da
woodstock <108785772+Stilllee@users.noreply.github.com>
2023-03-24 03:22:40 +0900

    📝 : "충돌해결"

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,21 @@
+// eslint-disable-next-line import/extensions
+export * from './FindId/FindIdResult';
+export * from './FindId/FindId.jsx';
+export * from './FindPassword/FindPassword';
+export * from './SignIn/SignInButton';
+export * from './SignIn/SignInInfo';
+export * from './SignIn/SignInInputForm';
+export * from './SignIn/SignInSection';
+export * from './SignIn/SignInTitle';
+export * from './Profiles/ProfileMain';
+export * from './Profiles/ProfileTitle';
+export * from './Profiles/ProfileMembers';
+export * from './Profiles/ProfileMemberList';
+export * from './Footer/CopyRightContents';
+export * from './Footer/SnsIcon';
+export * from './Footer/LinkAtag';
+export * from './Footer/TopInfo';
 export * from './Header/Menu';
 export * from './Header/Aside';
 export * from './Header/Search';
-// eslint-disable-next-line import/export
 export * from './Header/Profile';
