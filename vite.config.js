97e2c49e6fa58e39505c760cf389874985ab478c
LuckyDoyun <doyun.jang.0128@gmail.com>
2023-03-27 15:13:56 +0900

    vite.confing.js 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/LAB13-PROJECT/',
   resolve: {
     alias: [
       { find: '@', replacement: '/src' },
